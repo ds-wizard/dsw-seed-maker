5f6aa4194cb53c8cc0b6237f6996818b834f7d7c
jmartinkova <j.martinkova@outlook.com>
2024-11-05 17:58:48 +0100

    Users, projects importers, knowledge models lists works

--- src/dsw_seed_maker/logic.py
+++ src/dsw_seed_maker/logic.py
@@ -1,19 +1,43 @@
+from .comm import Database
 from .models import ExampleRequestDTO, ExampleResponseDTO
 from .comm.db import Database, DatabaseConnection
 from psycopg import sql
 from typing import List, Dict, Any
-import json
 
 def example_logic(req_dto: ExampleRequestDTO) -> ExampleResponseDTO:
     return ExampleResponseDTO(
         message=req_dto.message.replace('server', 'client'),
     )
 
-def list_resources_users_logic() -> list[dict[str, Any]]:
-    db = Database(name='postgres', dsn='postgresql://postgres:postgres@localhost:15432/wizard')
+def connect_to_db_logic() -> Database:
+    return Database(name='postgres', dsn='postgresql://postgres:postgres@localhost:15432/wizard')
+
+def list_logic(resource_type: str) -> dict[str, list[dict[str, Any]]] | list[dict[str, Any]]:
+    db = connect_to_db_logic()
+    if resource_type == 'all':
+        return list_all_logic(db)
+    elif resource_type == 'users':
+        return {'users' : list_users_logic(db)}
+    elif resource_type == 'projects_importers':
+        return {'projects_importers' : list_projects_importers_logic(db)}
+    elif resource_type == 'knowledge_models':
+        return {'knowledge_models' : list_knowledge_models_logic(db)}
+
+def list_all_logic(db) -> dict[str, list[dict[str, str | Any]] | list[dict[str, Any]]]:
+    users= list_users_logic(db)
+    projects_importers = list_projects_importers_logic(db)
+    knowledge_models = list_knowledge_models_logic(db)
+    resources = {
+        'users': users,
+        'projects_importers': projects_importers,
+        'knowledge_models': knowledge_models
+    }
+    return resources
+
+def list_users_logic(db) -> list[dict[str, str | Any]]:
     query = sql.SQL('SELECT * FROM user_entity')
-    users_resources = db.execute_query(query)
-    # Parse the dictionaries into UserDTO instances
+    resources = db.execute_query(query)
+    # Parse the dictionaries into instances
     parsed_resources = [
         {
             'uuid': str(row['uuid']),  # Convert UUID to string
@@ -21,7 +45,35 @@ def list_resources_users_logic() -> list[dict[str, Any]]:
             'last_name': row['last_name'],
             'role': row['role']
         }
-        for row in users_resources
+        for row in resources
     ]
+    return parsed_resources
 
+def list_projects_importers_logic(db) -> list[dict[str, Any]]:
+    query = sql.SQL('SELECT * FROM questionnaire_importer')
+    resources = db.execute_query(query)
+    # Parse the dictionaries into UserDTO instances
+    parsed_resources: list[dict[str, Any]] = [
+        {
+            'id': row['id'],  # Convert UUID to string
+            'name': row['name'],
+            'description': row['description']
+        }
+        for row in resources
+    ]
+    return parsed_resources
+
+def list_knowledge_models_logic(db) -> list[dict[str, Any]]:
+    query = sql.SQL('SELECT * FROM package')
+    resources = db.execute_query(query)
+    # Parse the dictionaries into UserDTO instances
+    parsed_resources = [
+        {
+            'id': row['id'],  # Convert UUID to string
+            'name': row['name'],
+            'km_id': row['km_id'],
+            'description': row['description']
+        }
+        for row in resources
+    ]
     return parsed_resources
