d1190abc82bc04bd4b30839c1b0fde05e98d1c8f
Jana Martínková <83722371+jmartinkova@users.noreply.github.com>
2024-11-07 11:41:40 +0100

    Recipe file creation (#10)

---
+++ tmp.js
@@ -0,0 +1,85 @@
+{
+    "users": [
+        {
+            "uuid": "e1c58e52-0824-4526-8ebe-ec38eec67030",
+            "first_name": "Isaac",
+            "last_name": "Newton",
+            "role": "researcher"
+        },
+        {
+            "uuid": "30d48cf4-8c8a-496f-bafe-585bd238f798",
+            "first_name": "Nikola",
+            "last_name": "Tesla",
+            "role": "dataSteward"
+        },
+        {
+            "uuid": "00000000-0000-0000-0000-000000000000",
+            "first_name": "System",
+            "last_name": "User",
+            "role": "admin"
+        },
+        {
+            "uuid": "ec6f8e90-2a91-49ec-aa3f-9eab2267fc66",
+            "first_name": "Albert",
+            "last_name": "Einstein",
+            "role": "admin"
+        }
+    ],
+    "project_importers": [
+        {
+            "id": "dsw:replies-importer:0.1.0",
+            "name": "DSW Replies (JSON)",
+            "description": "Import from replies in JSON exported from DSW"
+        }
+    ],
+    "knowledge_models": [
+        {
+            "id": "myorg:km-for-seeding:0.0.1",
+            "name": "KM for seeding",
+            "km_id": "km-for-seeding",
+            "description": ""
+        }
+    ],
+    "locales": [
+        {
+            "id": "wizard:default:1.0.0",
+            "name": "English",
+            "code": "en",
+            "description": "Default English locale for Wizard UI"
+        }
+    ],
+    "document_templates": [
+        {
+            "id": "myorg:dsw-seeding:0.0.1",
+            "name": "DSW seeding",
+            "template_id": "dsw-seeding"
+        },
+        {
+            "id": "myorg:dsw-seeding2:0.0.1",
+            "name": "DSW seeding 2",
+            "template_id": "dsw-seeding2"
+        },
+        {
+            "id": "myorg:dsw-seeding2:0.1.0",
+            "name": "DSW seeding 2",
+            "template_id": "dsw-seeding2"
+        },
+        {
+            "id": "dsw:questionnaire-report:2.12.0",
+            "name": "Questionnaire Report",
+            "template_id": "questionnaire-report"
+        }
+    ],
+    "projects": [
+        {
+            "uuid": "7ec5c86a-946a-4386-ba3c-b27481288a62",
+            "name": "Jana Mart\u00ednkov\u00e1"
+        }
+    ],
+    "documents": [
+        {
+            "uuid": "1034a4b0-d867-4b4b-b2a0-a3956b43cf95",
+            "name": "Jana Mart\u00ednkov\u00e1"
+        }
+    ]
+}
